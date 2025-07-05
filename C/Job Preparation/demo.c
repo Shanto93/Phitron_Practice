3591122f0c63537e3eae78502000c68305d097c3
Shanto93 <shantoislam7363@gmail.com>
2025-07-05 23:27:24 +0600

    lasdlk

---
+++ C/Job Preparation/demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[1001];
+    scanf("%s", &s);
+    int count = 0;
+
+    for(int i = 0; i< strlen(s); i++)
+    {
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+        {
+            count++;
+        }
+    }
+    printf("%d\n", count);
+}
